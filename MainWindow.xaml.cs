1c15ca9b63d28c224b996e2fe2ae416a682d40a6
Kseniya38 <shpakovakseniya38@gmail.com>
2023-05-14 12:49:55 +0800

    edited delete function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -43,9 +43,8 @@ namespace ExamsPerformance
 
             attestationsGrid.ItemsSource = attestationItemList;
         }
-        //var qwe = db.Attestation.Local.ToBindingList();
 
-        private void GridMouseUp(object sender, MouseButtonEventArgs e) // действие при одинарном клике на строку
+        private void GridMouseUp(object sender, MouseButtonEventArgs e)
         {
             AttestationItem selectedAttestation = attestationsGrid.SelectedItem as AttestationItem;
             //MessageBox.Show("Выбрана запись: \n Студент: " + selectedAttestation.Student.StudentFIO + "\n Преподаватель: " + selectedAttestation.Teacher.TeacherFIO + "\n Предмет: " + selectedAttestation.Subject.SubjectName + "\n Дата: " + selectedAttestation.AttestationDate, "Выбрана запись");
@@ -53,7 +52,6 @@ namespace ExamsPerformance
 
         private void AddButtonClick(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show("Нажали кнопку Добавить новую запись");
             AddWindow addWindow = new AddWindow();
             addWindow.Show();
             Hide();
@@ -61,7 +59,7 @@ namespace ExamsPerformance
 
         private void DeleteButtonClick(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show("Нажали кнопку Удалить");
+
             AttestationItem selectedAttestation = attestationsGrid.SelectedItem as AttestationItem;
             if (MessageBox.Show("Вы уверены, что хотите удалить следующую запись? Отменить это действие будет невозможно." + "\n Студент: " + selectedAttestation.Student.StudentFIO + "\n Преподаватель: " + selectedAttestation.Teacher.TeacherFIO + "\n Предмет: " + selectedAttestation.Subject.SubjectName + "\n Дата: " + selectedAttestation.AttestationDate,
                     "Предупреждение",
@@ -72,19 +70,30 @@ namespace ExamsPerformance
                 MessageBox.Show($"StudentId: {attestation.StudentId}, SubjectId: {attestation.SubjectId}, TeacherId: {attestation.TeacherId}");
                 db.Attestation.Remove(attestation);
                 db.SaveChanges();
-                attestationsGrid.Items.Refresh();
+            }
+            List<Attestation> attestationData = db.Attestation.ToList();
+            List<AttestationItem> attestationItemList = new List<AttestationItem>();
+
+            for (int i = 0; i < attestationData.Count; i++)
+            {
+                student = db.Student.Find(attestationData[i].StudentId);
+                teacher = db.Teacher.Find(attestationData[i].TeacherId);
+                subject = db.Subject.Find(attestationData[i].SubjectId);
+
+                attestationItemList.Add(new AttestationItem(student, teacher, subject, attestationData[i].AttestationDate, attestationData[i].AttestationTypeName, attestationData[i].Mark, attestationData[i].Result));
             }
 
+            attestationsGrid.ItemsSource = attestationItemList;
+            attestationsGrid.Items.Refresh();
         }
 
         private void SaveChagesButtonClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Нажали кнопку Cохранить изменения");
+            MessageBox.Show("Нажали кнопку Изменить запись");
         }
 
         private void CreateDocButtonClick(object sender, RoutedEventArgs e)
         {
-            //MessageBox.Show("Нажали кнопку Сформировать документ");
             CreateDocWindow addWindow = new CreateDocWindow();
             addWindow.Show();
         }
